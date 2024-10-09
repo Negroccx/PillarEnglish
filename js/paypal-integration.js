426f326eae57dea9341fa96a3dbe0d98a8663298
Negroccx <cnvega.19@gmail.com>
2024-10-09 15:37:45 -0300

    Add files via upload

--- js/paypal-integration.js
+++ js/paypal-integration.js
@@ -14,4 +14,55 @@
                 alert('Pago realizado por ' + details.payer.name.given_name);
             });
         }
-    }).render('#paypal-button-container');
+    }).render('#paypal-button-container1');
+
+    paypal.Buttons({
+        createOrder: function(data, actions) {
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: '50.00'
+                    }
+                }]
+            });
+        },
+        onApprove: function(data, actions) {
+            return actions.order.capture().then(function(details) {
+                alert('Pago realizado por ' + details.payer.name.given_name);
+            });
+        }
+    }).render('#paypal-button-container2');
+
+    paypal.Buttons({
+        createOrder: function(data, actions) {
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: '50.00'
+                    }
+                }]
+            });
+        },
+        onApprove: function(data, actions) {
+            return actions.order.capture().then(function(details) {
+                alert('Pago realizado por ' + details.payer.name.given_name);
+            });
+        }
+    }).render('#paypal-button-container3');
+
+    paypal.Buttons({
+        createOrder: function(data, actions) {
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: '50.00'
+                    }
+                }]
+            });
+        },
+        onApprove: function(data, actions) {
+            return actions.order.capture().then(function(details) {
+                alert('Pago realizado por ' + details.payer.name.given_name);
+            });
+        }
+    }).render('#paypal-button-container4');
